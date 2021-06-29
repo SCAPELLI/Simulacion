fa7dcb812f956c278206b78c13e9398878f02375
sebastian <scapelli@fi.uba.ar>
2021-06-29 20:24:02 -0300

    corrijo bug por el que no buscaba bien por año

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,8 +3,9 @@ require 'net/http'
 class MoviesController < ApplicationController
   def result
     title = params[:movie][:title]
-    omdb = "http://www.omdbapi.com/?apikey=e4c4b604"
-    url = URI.parse(omdb + '&t=' + title)
+    year = params[:movie][:year]
+    omdb = "http://www.omdbapi.com/?apikey=e4c4b604&plot=full"
+    url = URI.parse(omdb + '&t=' + title + '&y=' + year)
     req = Net::HTTP::Get.new(url.to_s)
     res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
