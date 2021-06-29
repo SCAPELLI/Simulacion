53f9782bbe1a072be1c3af8815c6d5695fbe18c9
sebastian <scapelli@fi.uba.ar>
2021-06-29 18:53:02 -0300

    agrego buscador de pelis, falta pulir

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,11 +5,25 @@ class MoviesController < ApplicationController
     unless session[:user_id]
       redirect_to root_path
     end
-    url = URI.parse('http://www.omdbapi.com/?apikey=e4c4b604&t=grand+blue')
+    omdb = "http://www.omdbapi.com/?apikey=e4c4b604"
+    url = URI.parse(omdb + '&t=grand+blue')
     req = Net::HTTP::Get.new(url.to_s)
     res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
     }
     @movie = ActiveSupport::JSON.decode(res.body)
+  end
+
+  def seeker
+    title = params[:movie][:title]
+    omdb = "http://www.omdbapi.com/?apikey=e4c4b604"
+    url = URI.parse(omdb + '&t=' + title)
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    @movie = ActiveSupport::JSON.decode(res.body)
+    # redirect_to "/movies"
+    render :seeker
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root "sessions#login"
   get "/movies", to: "movies#index"
 
+  get "/movies/filters", to: "movies#filters"
+  post "/movies/seeker", to: "movies#seeker"
+
+  get "/movies/seeker", to: "movies#seeker"
 
   #Users routes:
   resources :users, only: [:new, :create]
