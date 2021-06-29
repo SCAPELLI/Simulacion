b3399f20e75b5038820388d75400b525bb290df2
sebastian <scapelli@fi.uba.ar>
2021-06-29 19:41:08 -0300

    reordeno el controlador de movies y las views del buscador de peliculas

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,7 @@
 require 'net/http'
 
 class MoviesController < ApplicationController
-  def index
-    unless session[:user_id]
-      redirect_to root_path
-    end
-    omdb = "http://www.omdbapi.com/?apikey=e4c4b604"
-    url = URI.parse(omdb + '&t=grand+blue')
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)
-    }
-    @movie = ActiveSupport::JSON.decode(res.body)
-  end
-
-  def seeker
+  def result
     title = params[:movie][:title]
     omdb = "http://www.omdbapi.com/?apikey=e4c4b604"
     url = URI.parse(omdb + '&t=' + title)
@@ -23,7 +10,6 @@ class MoviesController < ApplicationController
       http.request(req)
     }
     @movie = ActiveSupport::JSON.decode(res.body)
-    # redirect_to "/movies"
-    render :seeker
+    render :result
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   root "sessions#login"
   get "/movies", to: "movies#index"
 
-  get "/movies/filters", to: "movies#filters"
-  post "/movies/seeker", to: "movies#seeker"
-
   get "/movies/seeker", to: "movies#seeker"
+  post "/movies/seeker/result", to: "movies#result"
 
   #Users routes:
   resources :users, only: [:new, :create]
