ae9258ce6d3e0f29e25fd9bd36758cb3eca27eee
sebastian <scapelli@fi.uba.ar>
2021-06-30 01:55:36 -0300

    cambio el modo de busqueda por titulo a busqueda por similares, quedó muy bonito :)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,39 @@
 require 'net/http'
 
 class MoviesController < ApplicationController
-  def result
-    title = params[:movie][:title]
-    year = params[:movie][:year]
+  def results
+    if params[:movie] != nil
+      $title = params[:movie][:title]
+      $year = params[:movie][:year]
+    end
     omdb = "http://www.omdbapi.com/?apikey=e4c4b604&plot=full"
-    url = URI.parse(omdb + '&t=' + title + '&y=' + year)
+    url = URI.parse(omdb + '&s=' + $title + '&y=' + $year)
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    @response = ActiveSupport::JSON.decode(res.body)
+    if @response["Response"] == "True"
+      @movies = @response["Search"]
+      render :results
+    else
+      redirect_to "/movies/seeker"
+    end
+  end
+  
+  def find_by_id
+    id = params[:id]
+    omdb = "http://www.omdbapi.com/?apikey=e4c4b604&plot=full"
+    url = URI.parse(omdb +'&i=' + id)
     req = Net::HTTP::Get.new(url.to_s)
     res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
     }
     @movie = ActiveSupport::JSON.decode(res.body)
-    render :result
+    if @movie["Response"] == "True"
+      render :details
+    else
+      redirect_to "/movies/seeker"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root "sessions#login"
-  get "/movies", to: "movies#index"
 
   get "/movies/seeker", to: "movies#seeker"
-  post "/movies/seeker/result", to: "movies#result"
+
+  get "/movies/seeker/result", to: "movies#results"
+  post "/movies/seeker/result", to: "movies#results"
+
+  get "/movies/id/:id", to: "movies#find_by_id"
 
   #Users routes:
   resources :users, only: [:new, :create]
