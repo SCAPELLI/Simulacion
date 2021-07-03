37481a6e588212cf0fa9f62a64bd2af828bacf0c
diegobalestieri <diego.balestieri1998@gmail.com>
2021-07-03 12:21:48 -0300

    paso navbar a todas las pantallas y finalizo agregar pelicula a favoritos

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,14 @@ class MoviesController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @movie = @user.movies.create(movie_params)
+    redirect_to request.referrer
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @movie = @user.movies.where(tmdbId: params[:tmdbId]).first
+    @movie.destroy
+    redirect_to request.referrer
   end
 
   private
